e5fe2ca1f31a8a90acb779079ecb457a96f5d745
Kazuhiro Sasaki <givingbackks@proton.me>
2023-04-20 18:45:03 +0900

    Initial Commit

---
+++ GP-ChatMate/GP_ChatMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GP_ChatMateApp.swift
+//  GP-ChatMate
+//
+//  Created by myruei on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct GP_ChatMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
